abcc9485a25afe4b55dfc3b4ade7aeb1e83e2971
Brian Ellis <be4833@ochmcomp01.ccbb.utexas.edu>
2019-01-15 15:47:34 -0600

    removed post examples

--- 3_bacteria_post/_conspecifix/database/User_spec/make_hm_graph_points.R
+++
@@ -1,9 +0,0 @@
-png('/stor/work/Ochman/brian/examples/conExample3/_conspecifix/database/User_spec/gno2.png' ,width = 6,
-  height    = 6,
-  units     = "in",
-  res       = 200)
-par(mfrow=c(1,1))
-tab = read.table('/stor/work/Ochman/brian/examples/conExample3/_conspecifix/database/User_spec/graph_points.txt')
-plot(tab$V1,tab$V2,xlab=c("# Genomes"),ylab=c("h/m"),main='User_spec')
-abline(h=0.108065507964,col="red", lwd=2, lty=2)
-abline(h=0.196154000259,col="red", lwd=1, lty=3)
--- 3_bacteria_post/_conspecifix/scripts/database/child_launcher.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-python child_runner.py
